a7b788b3493e062b6046dc7b37e62bef4f75a04c
Morten Houmøller Nygaard <morten@mortz.dk>
2017-03-03 21:44:26 +0100

    Bump to 1.0.1

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
  * The ColorError class is used whenever the parsing of a color goes wrong.
  *
  * @module ColorInterpreter
- * @version 1.0.0
+ * @version 1.0.1
  *
  * @example
  * // A typical usage of the Color object. Here the "gold" string could be
